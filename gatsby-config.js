7e3805076f26e5689bb013f92031f84702438dd3
Abdul-razak Twaha (DHL TZ) <Abdul-Razak.Twaha@dhl.co.tz>
2022-12-31 22:38:59 +0300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,13 @@ module.exports = {
     title: `gatsby-tailwind`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }, "gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-mdx", `gatsby-plugin-image`, `gatsby-plugin-sharp`, `gatsby-transformer-sharp`, "gatsby-plugin-postcss",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p className='font-mono font-bold italic'>If there's an Address, we'll find it..!</p>
+      <StaticImage alt="If there's an Address, we'll find it" src="../images/home.jpg" />
     </Layout>
   )
 }
