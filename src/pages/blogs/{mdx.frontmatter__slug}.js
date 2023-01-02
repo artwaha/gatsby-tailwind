f5ed1382e57826377b6d86caa5fa846c59054e79
Abdul-razak Twaha (DHL TZ) <Abdul-Razak.Twaha@dhl.co.tz>
2023-01-02 13:48:24 +0300

    Finished Gatsby Tutorial Part 7

--- src/pages/blogs/{mdx.frontmatter__slug}.js
+++ src/pages/blogs/{mdx.frontmatter__slug}.js
@@ -1,13 +1,18 @@
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPost = ({ data, children }) => {
   // console.log(props);
+
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
+    <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage alt={data.mdx.frontmatter.hero_image_alt} image={image} />
       {children}
     </Layout>
   );
@@ -16,11 +21,17 @@ const BlogPost = ({ data, children }) => {
 export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
 
 export const query = graphql`
-  query ($id: String) {
+  query MyQuery($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
